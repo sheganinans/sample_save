031c80c1385f4f727b4db437fe8838bca6ab6d37
Aistis Raulinaitis <sheganinans@gmail.com>
2022-06-20 18:58:44 -0700

    base64 encoded float vectors

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import base64
 from fastapi import FastAPI
 import hashlib
 from pydantic import BaseModel
-from typing import List
+import struct
 
 app = FastAPI()
-con = apsw.Connection("samples.sqlite")
+con = apsw.Connection("samples_v2.sqlite")
 
 
 class Sample(BaseModel):
   seed: int
-  vector: List[float]
+  vec: str
   img: str
 
 
@@ -19,12 +19,20 @@ class Sample(BaseModel):
 async def sample(s: Sample):
   try:
     img = base64.b64decode(s.img)
+    # TODO:
+    #vec = base64.b64decode(s.vec)
+    #vec = struct.unpack("f", vec)
+    #if len(vec) != 512:
+    #  raise Exception(f"Expecting length 512, got: {len(vec)}")
+    #if not all([x >= -1. and x <= 1. for x in vec]):
+    #  # TODO: Better error.
+    #  raise Exception(f"Data Malformed.")
     md5 = hashlib.md5(img).hexdigest()
     with open(f"./samples/{md5}.png", "wb") as f:
       f.write(img)
     with con:
       con.cursor().execute(f"""insert into samples (seed, vector, md5) values \
-                                ({s.seed},"{s.vector}","{md5}")""")
+                                ({s.seed},"{s.vec}","{md5}")""")
     return {"ok": "!"}
   except Exception as e:
     with open("./err.log", "a+") as errs:
