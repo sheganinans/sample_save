9fa34ba6eacfe9a141fe6becaa51e5c88330cfaf
Aistis Raulinaitis <sheganinans@gmail.com>
2022-07-02 16:20:24 -0700

    new protocol

--- main.py
+++ main.py
@@ -4,37 +4,40 @@ import base64
 from fastapi import FastAPI, HTTPException
 import hashlib
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 
 app = FastAPI()
 con = apsw.Connection("samples.sqlite")
 
 
-class Sample(BaseModel):
-  seed: int
+class Samples(BaseModel):
   vector: List[float]
-  img: str
+  imgs: Dict[int, str]
+
+
+cols, qs = ",".join([f"c{i+1}" for i in range(512)]), ",".join(["?" for _ in range(512)])
+insert_into_vector = f"""insert into vector ({cols}) values ({qs})"""
 
 
 @app.post("/sample")
-async def sample(s: Sample):
+async def sample(s: Samples):
   if s.seed < 0 or s.seed > 18446744073709551615:  # sys.maxsize*2+1
     raise HTTPException(status_code=400, detail=f"Seed either negative or too large.")
   if len(s.vector) != 512:
     raise HTTPException(status_code=400, detail=f"Expecting vector of length 512, got: {len(s.vector)}")
-  # TODO: Normalize, preserve, or error?
-  #if not all([x >= -1. and x <= 1. for x in s.vector]):
-  #  raise HTTPException(status_code=400, detail=f"Vector not properly normalized.")
   try:
-    img = base64.b64decode(s.img)
+    imgs = [base64.b64decode(img) for img in s.imgs]
   except binascii.Error:
-    raise HTTPException(status_code=400, detail="The image's base64 encoding was malformed.")
+    raise HTTPException(status_code=400, detail=f"An image's base64 encoding was malformed.")
   try:
-    md5 = hashlib.md5(img).hexdigest()
-    with open(f"./samples/{md5}.png", "wb") as f:
-      f.write(img)
     with con:
-      con.cursor().execute("insert into samples (seed, vector, md5) values (?,?,?)", (s.seed, str(s.vector), md5))
+      con.execute(insert_into_vector, tuple(s.vector))
+      vec_id = con.last_insert_rowid()
+      for img in imgs:
+        md5 = hashlib.md5(base64.b64decode(img)).hexdigest()
+        with open(f"./samples/{md5}.png", "wb") as f:
+          f.write(img)
+        con.execute("insert into sample (seed, md5, vector) values (?,?,?)", (s.seed, md5, vec_id))
     return {"ok": "!"}
   except Exception as e:
     with open("./err.log", "a+") as errs:
