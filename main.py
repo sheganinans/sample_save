a9a33a35f02e958aba2f2fab4857955b27cb78e3
Aistis Raulinaitis <sheganinans@gmail.com>
2022-07-02 16:25:06 -0700

    protocol updates

--- main.py
+++ main.py
@@ -21,23 +21,24 @@ insert_into_vector = f"""insert into vector ({cols}) values ({qs})"""
 
 @app.post("/sample")
 async def sample(s: Samples):
-  if s.seed < 0 or s.seed > 18446744073709551615:  # sys.maxsize*2+1
-    raise HTTPException(status_code=400, detail=f"Seed either negative or too large.")
+  for seed, _ in s.imgs.items():
+    if seed < 0 or seed > 18446744073709551615:  # sys.maxsize*2+1
+      raise HTTPException(status_code=400, detail=f"A seed was either negative or too large.")
   if len(s.vector) != 512:
     raise HTTPException(status_code=400, detail=f"Expecting vector of length 512, got: {len(s.vector)}")
   try:
-    imgs = [base64.b64decode(img) for img in s.imgs]
+    imgs = {seed: base64.b64decode(img) for seed, img in s.imgs.items()}
   except binascii.Error:
     raise HTTPException(status_code=400, detail=f"An image's base64 encoding was malformed.")
   try:
     with con:
-      con.execute(insert_into_vector, tuple(s.vector))
+      con.cursor().execute(insert_into_vector, tuple(s.vector))
       vec_id = con.last_insert_rowid()
-      for img in imgs:
+      for seed, img in imgs.items():
         md5 = hashlib.md5(base64.b64decode(img)).hexdigest()
         with open(f"./samples/{md5}.png", "wb") as f:
           f.write(img)
-        con.execute("insert into sample (seed, md5, vector) values (?,?,?)", (s.seed, md5, vec_id))
+        con.cursor().execute("insert into sample (seed, md5, vector) values (?,?,?)", (seed, md5, vec_id))
     return {"ok": "!"}
   except Exception as e:
     with open("./err.log", "a+") as errs:
